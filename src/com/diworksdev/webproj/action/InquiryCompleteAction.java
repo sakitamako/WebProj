0238d5f5af7c798a85f2ccd589b475fbc9d1e1b4
sakitamako <sa.919.1025.ki@gmail.com>
2024-06-03 21:48:18 +0900

    コメント

--- src/com/diworksdev/webproj/action/InquiryCompleteAction.java
+++ src/com/diworksdev/webproj/action/InquiryCompleteAction.java
@@ -2,35 +2,52 @@ package com.diworksdev.webproj.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+//struts2が持つActionSupportというクラスを継承
+//（Actionクラスは基本的にこのクラスを継承）
+//LoginAciton（子クラス） extends（継承） ActionSupport（親クラス）
+//すでにあるクラスとにたクラスを作る場合、元のクラスに必要な機能だけを追加する形で、新しいクラスを作ることを継承
 public class InquiryCompleteAction extends ActionSupport {
 	private String name;
 	private String qtype;
 	private String body;
 
+	//管理コマンド・メッセージをコマンド・サーバーに送信し、何らかの応答メッセージを待ちます
 	public String execute() {
 		return SUCCESS;
 	}
 
+	//getterは値を引っ張ってくるメソッド
+	//メソッド＝オブジェクトの操作を定義したもの
 	public String getName() {
 		return name;
 	}
 
+	//setterは値を代入するメソッド
+	//メソッド＝オブジェクトの操作を定義したもの
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	//getterは値を引っ張ってくるメソッド
+	//メソッド＝オブジェクトの操作を定義したもの
 	public String getQtype() {
 		return qtype;
 	}
 
+	//setterは値を代入するメソッド
+	//メソッド＝オブジェクトの操作を定義したもの
 	public void setQtype(String qtype) {
 		this.qtype = qtype;
 	}
 
+	//getterは値を引っ張ってくるメソッド
+	//メソッド＝オブジェクトの操作を定義したもの
 	public String getBody() {
 		return body;
 	}
 
+	//setterは値を代入するメソッド
+	//メソッド＝オブジェクトの操作を定義したもの
 	public void setBody(String body) {
 		this.body = body;
 	}
